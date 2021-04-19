b83de91aa8a30ee8cc65fc26bf75f2011a54e1f9
matcampos <matheus.alves1998@hotmail.com>
2021-04-18 22:21:55 -0300

    Standardizing imports

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+import * as propertiesController from './properties.controller';
+
+export {
+    propertiesController
+}
--- src/routes/properties.route.ts
+++ src/routes/properties.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import { findProperties } from '../controllers/properties.controller';
+import { propertiesController } from '../controllers';
 import { validate } from 'express-validation';
 import paramValidation from '../helpers/param-validation';
 import { pagination } from '../middlewares';
+
 const routes = Router();
 
 routes
     .route('/')
-    .get(validate(paramValidation.findProperties), pagination, findProperties);
+    .get(validate(paramValidation.findProperties), pagination, propertiesController.findProperties);
 
 export default routes;
